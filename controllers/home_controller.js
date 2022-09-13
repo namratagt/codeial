3936c0258d8430e51b93e95b92930b73fe2f0c13
namrata <gnamrata066@gmail.com>
2022-09-13 19:55:18 +0530

    setting the view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,9 @@ module.exports.home = function(req, res){
     return res.end('<h1>Express is up for codeial</h1>')
 }
 
+module.exports.home2 = function(req, res){
+    return res.end('<h1>home2</h1>')
+}
 
 
 //modeule.exports.actionName = function(req, res){}
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+
 console.log('router loaded');
 
-
-
-
-
 router.get('/', homeController.home); 
+router.get('/h2', homeController.home2);
+router.use('/users', require('./users'));
+router.use('/posts', require('./post'));
+
+
+//for any further routes access from here
+//router.use('routerName', require('./routerfile));
+
 
 module.exports = router;
